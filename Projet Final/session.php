a8010d02dbdc477e18a7dba9e12b80b2983e4504
elliot-bordrez <90181718+elliot-bordrez@users.noreply.github.com>
2022-08-26 10:30:35 +0200

    Add files via upload

--- Projet Final/session.php
+++ Projet Final/session.php
@@ -1,6 +1,17 @@
 <?php session_start(); 
 include("classes/User.php");
 include("classes/Stade.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+    <!-- css -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">   
+</head>
+<body>
+<?php
 
 $TheUser = new User(null,null,null);
 
@@ -40,14 +51,54 @@ try {
         <?php
     }else{
         echo "Veuillez vous identifier";
-        ?>
-        <form action="" method="post">
-            Login : <input type="text" name="login" value="elliot"/>
-            Pass : <input type="password" name="pass" value="elliot"/>
-            <input type="submit" name="connexion">
-        </form>
-        <?php
-    }
-
-
     ?>
+<div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Sign In</h3>
+				<div class="d-flex justify-content-end social_icon">
+					<span><i class="fab fa-facebook-square"></i></span>
+					<span><i class="fab fa-google-plus-square"></i></span>
+					<span><i class="fab fa-twitter-square"></i></span>
+				</div>
+			</div>
+			<div class="card-body">
+				<form action="" method="post">
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="text" class="form-control" placeholder="username" name="login">
+						
+					</div>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+						<input type="password" class="form-control" placeholder="password" name="pass">
+					</div>
+					<div class="row align-items-center remember">
+						<input type="checkbox">Remember Me
+					</div>
+					<div class="form-group">
+						<input type="submit" value="Login" class="btn float-right login_btn" name="connexion">
+					</div>
+				</form>
+			</div>
+			<div class="card-footer">
+				<div class="d-flex justify-content-center links">
+					Don't have an account?<a href="#">Sign Up</a>
+				</div>
+				<div class="d-flex justify-content-center">
+					<a href="#">Forgot your password?</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+}
+?>
+</body>
+</html>
---
+++ Projet Final/test.php
@@ -0,0 +1,83 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+    
+   <!--Made with love by Mutiullah Samim -->
+   
+	<!--Bootsrap 4 CDN-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+	<!--Custom styles-->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+</head>
+<body>
+    <?php include("session.php");
+
+    if(isset($_SESSION['Connexion'])){
+    ?>
+    <h1> Index </h2>
+    <div> Bienvenu <?php echo $TheUser->getLogin()?></div>
+
+    <?php
+        if($TheUser->isAdmin()){
+            echo"vous etes admin";
+        }else{
+            echo "vous etes un simple visiteur";
+        }
+    ?>
+<div class="container">
+    <h3 class="h3">shopping Demo-1 </h3>
+    <div class="row">
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid">
+                <div class="product-image">
+                    <a href="#">
+                        <img class="pic-1" src="http://bestjquery.com/tutorial/product-grid/demo9/images/img-1.jpg">
+                        <img class="pic-2" src="http://bestjquery.com/tutorial/product-grid/demo9/images/img-2.jpg">
+                    </a>
+                    <ul class="social">
+                        <li><a href="" data-tip="Quick View"><i class="fa fa-search"></i></a></li>
+                        <li><a href="" data-tip="Add to Wishlist"><i class="fa fa-shopping-bag"></i></a></li>
+                        <li><a href="" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+                    </ul>
+                    <span class="product-new-label">Sale</span>
+                    <span class="product-discount-label">20%</span>
+                </div>
+                <ul class="rating">
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star disable"></li>
+                </ul>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Women's Blouse</a></h3>
+                    <div class="price">$16.00
+                        <span>$20.00</span>
+                    </div>
+                    <a class="add-to-cart" href="">+ Add To Cart</a>
+                </div>
+            </div>
+        </div>
+    <?php
+    }
+
+    //affichage des stades
+    $Stade = new Stade(null,null,null,null);
+    $tabStades = $Stade->getAllStade();
+    echo"<ul>";
+    foreach ($tabStades as $lestade) {
+        $lestade->renderHTML();
+    }
+    echo "</ul>";
+    
+?>
+</body>
+</html>
